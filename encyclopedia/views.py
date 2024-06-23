1b1a453dec314b0ad3b433ab48de95c78383a4f8
TA GIA BAO <git@github.com>
2024-06-24 00:02:47 +0700

    update: error page, entry page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from . import util
 
@@ -8,3 +9,9 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def view_entry(request, entry):
+    if util.get_entry(entry) == None:
+        return render(request, "encyclopedia/not_found.html", {
+            "entry": entry
+        })
+    return HttpResponse(f"Entry: {entry}")
